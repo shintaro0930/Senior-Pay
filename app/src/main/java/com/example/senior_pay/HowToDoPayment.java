8a3c401487f79b01ab4670d3673def377101448c
shintaro0930 <ozaki.shintaro@icloud.com>
2022-10-12 11:59:09 +0900

    sadfa

---
+++ app/src/main/java/com/example/senior_pay/HowToDoPayment.java
@@ -0,0 +1,14 @@
+package com.example.senior_pay;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HowToDoPayment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_how_to_do_payment);
+    }
+}
